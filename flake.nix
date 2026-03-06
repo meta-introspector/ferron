3b06cd7e847d215ac56556a9542d91e1e95e7855
mike dupont <gemini-agent-47@solfunmeme.com>
2026-03-06 16:58:06 -0500

    Clean flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
-  description = "Ferron 2.x for ZOS";
+  description = "Ferron 2.x";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
@@ -7,10 +9,20 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages.${system}.default = pkgs.runCommand "ferron-2.5.5" {} ''
-        mkdir -p $out/bin
-        cp ${./target/release/ferron} $out/bin/ferron
-        chmod +x $out/bin/ferron
-      '';
-    };
-}
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "ferron";
+        version = "2.5.5";
+        src = ./.;
+        
+        buildFeatures = [ "config-yaml-legacy" "runtime-tokio" ];
+        
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "async-compression-0.4.36" = "sha256-5rNEP5A7ahy+wv2U+lkGrG1ewVFhoFREsgeiQyXZzno=";
+            "cache_control-0.2.0" = "sha256-Xw8JMo5bCgLfOsjsdyOxl956ggjWqywoQZA8Liz7bKE=";
+            "dns-update-0.1.6" = "sha256-V5mUHWj6qAxRVCuQ6/XyvB992iJK22f5R+YNn1/BS+I=";
+            "monoio-0.2.4" = "sha256-SnYzht1J3NedIjHK0MJVZFFWfjsZ42Dwnk2UJzFN8ZQ=";
+          };
+        };
+        
